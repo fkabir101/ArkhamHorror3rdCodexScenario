be1d92f932f914064596f6176dddc1f8c6ad7712
Fahim Kabir <fkabir101@gmail.com>
2019-06-26 14:48:46 -0400

    scenario json displayed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import ScenarioCard from "./components/ScenarioCard"
+import ScenarioCard from "./components/ScenarioCard";
+import Scenarios from "./json/scenario.json";
 
 class App extends Component {
   render(){
     return(
       <div className="App">
-        <ScenarioCard></ScenarioCard>
+        {Scenarios.map(scenario => {
+          return <ScenarioCard id={scenario.id}></ScenarioCard>
+        })}
       </div>
     )
   }
--- src/components/ScenarioCard.js
+++ src/components/ScenarioCard.js
@@ -1,9 +1,20 @@
 import  React, { Component } from 'react';
-
+import Scenarios from "../json/scenario.json";
 class ScenarioCard extends Component{
+  state = {
+    scenarioId : this.props.id
+  }
   render(){
     return(
-    <h1>ScenarioCard</h1>
+      <div className="card col-6">
+        <div className = "content">
+          <h1>{Scenarios[this.state.scenarioId].name}</h1>
+          <h4>Reconing: {Scenarios[this.state.scenarioId].reconing}</h4>
+          <p>The story so far:</p>
+          <p>{Scenarios[this.state.scenarioId].text}</p>
+          <p>Starting Location: {Scenarios[this.state.scenarioId].starting}</p>
+        </div>
+      </div>
     )
   }
 }
