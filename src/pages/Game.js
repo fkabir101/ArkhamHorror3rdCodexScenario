d2fab5c0a56df845f2657375359f367bea08e750
Fahim Kabir <fkabir101@gmail.com>
2019-07-17 13:13:49 -0400

    created system to display correct scenario card

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,12 +1,36 @@
 import React, {Component} from "react";
-// import ScenarioCard from "../components/ScenarioCard";
-// import Scenarios from "../json/scenario.json"
+import ScenarioCard from "../components/ScenarioCard";
+import Scenarios from "../json/scenario.json"
 
 class GamePage extends Component{
+  state = {
+    clues : 0,
+    doom : 0,
+    scenarioId : 0,
+    scenario : "",
+    scenarioMounted : false
+  }
+  componentDidMount = () =>{
+    for(let i = 0; i < Scenarios.length; i++){
+      if (Scenarios[i].scenario === this.props.match.params.scenario){
+        this.setState({scenario : Scenarios[i], scenarioId : i,
+        scenarioMounted : true});
+      }
+    }
+  }
   render(){
+    let mounted = false 
+    if(this.state.scenarioMounted){
+      mounted = true;
+    }
     return (
       <div>
-        <p>{this.props.match.params.scenario}</p>
+        {mounted ? (
+          <div className = "d-flex justify-content-center">
+            <ScenarioCard id = {this.state.scenarioId}></ScenarioCard>
+          </div>
+        )
+        : (<h1>Loading</h1>)}
       </div>
     )
   }
